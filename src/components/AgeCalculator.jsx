7c9cb747f7502c339c8cbc1be84c02d5af0a037f
Om Preetham <ompreethambandi@gmail.com>
2024-04-04 16:47:04 -0500

    designed html and setup scss files

--- src/components/AgeCalculator.jsx
+++ src/components/AgeCalculator.jsx
@@ -1,4 +1,64 @@
 const AgeCalculator = () => {
-  return <div>AgeCalculator</div>
+  return (
+    <main className="wrapper wrapper-center">
+      <section className="form-wrapper">
+        <form className="form" action="">
+          <article className="form__item">
+            <label className="item__label" htmlFor="day">
+              day
+            </label>
+            <input
+              className="item__input"
+              type="text"
+              id="day"
+              name="day"
+              placeholder="dd"
+            />
+          </article>
+          <article className="form__item">
+            <label className="item__label" htmlFor="month">
+              month
+            </label>
+            <input
+              className="item__input"
+              type="text"
+              id="month"
+              name="month"
+              placeholder="mm"
+            />
+          </article>
+          <article className="form__item">
+            <label className="item__label" htmlFor="year">
+              year
+            </label>
+            <input
+              className="item__input"
+              type="text"
+              id="year"
+              name="year"
+              placeholder="yyyy"
+            />
+          </article>
+          <button type="button">
+            <img
+              src="/images/icon-arrow.svg"
+              alt="Down facing arrow icon for submit"
+            />
+          </button>
+        </form>
+      </section>
+      <section className="results results-wrapper">
+        <p className="results__item">
+          <span className="results__answer">--</span>years
+        </p>
+        <p className="results__item">
+          <span className="results__answer">--</span>months
+        </p>
+        <p className="results__item">
+          <span className="results__answer">--</span>days
+        </p>
+      </section>
+    </main>
+  )
 }
 export default AgeCalculator
