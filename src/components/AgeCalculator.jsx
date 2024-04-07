5f6c7430493cfd99eaf45877c782978150d102ea
Om Preetham <ompreethambandi@gmail.com>
2024-04-07 18:13:35 -0500

    created states to manage dates

--- src/components/AgeCalculator.jsx
+++ src/components/AgeCalculator.jsx
@@ -1,55 +1,119 @@
+import { useState } from 'react'
+
 const AgeCalculator = () => {
+  // REGEX
+
+  const DAY_REGEX = /^(0?[1-9]|[1-2][0-9]|3[01])$/
+  const MONTH_REGEX = /^(0?[1-9]|1[0-2])$/
+  const YEAR_REGEX = /^\d{4}$/
+
+  // RESULTS
+
+  const [years, setYears] = useState('- -')
+  const [months, setMonths] = useState('- -')
+  const [days, setDays] = useState('- -')
+
+  // INPUTS
+
+  const [inputDay, setInputDay] = useState('')
+  const [inputMonth, setInputMonth] = useState('')
+  const [inputYear, setInputYear] = useState('')
+
+  console.log(inputDay, inputMonth, inputYear)
+
+  // ERROR?
+
+  const [errorDay, setErrorDay] = useState(false)
+  const [errorMonth, setErrorMonth] = useState(false)
+  const [errorYear, setErrorYear] = useState(false)
+
+  // ERROR MESSAGES
+
+  const [errorDayMessage, setErrorDayMessage] = useState('')
+  const [errorMonthMessage, setErrorMonthMessage] = useState('')
+  const [errorYearMessage, setErrorYearMessage] = useState('')
+
+  // CURRENT DATES
+
+  const currentDate = new Date()
+
+  const currentDay = currentDate.getDate()
+  const currentMonth = currentDate.getMonth() + 1
+  const currentYear = currentDate.getFullYear()
+
+  console.log(currentDay, currentMonth, currentYear)
+
+  // UPDATE INPUTS
+
+  const updateDay = (day) => {}
+
+  const updateMonth = (month) => {}
+
+  const updateYear = (year) => {}
+
   return (
     <main className="wrapper">
       <section className="form-wrapper">
         <form className="form" action="">
           <article className="form__item">
-            <label className="item__label" htmlFor="day">
+            <label
+              className={`item__label ${errorDay && 'label-error'}`}
+              htmlFor="day"
+            >
               day
             </label>
             <input
-              className="item__input"
+              onChange={(e) => updateDay(e.target.value)}
+              className={`item__input ${errorDay && 'input-error'}`}
               type="number"
               inputMode="numeric"
               min={1}
-              max={2}
+              max={31}
               id="day"
               name="day"
               placeholder="dd"
             />
-            <p className="error-message">This feild is required</p>
+            <p className="error-message">{errorDayMessage}</p>
           </article>
           <article className="form__item">
-            <label className="item__label" htmlFor="month">
+            <label
+              className={`item__label ${errorMonth && 'label-error'}`}
+              htmlFor="month"
+            >
               month
             </label>
             <input
-              className="item__input"
+              onChange={(e) => updateMonth(e.target.value)}
+              className={`item__input ${errorMonth && 'input-error'}`}
               type="number"
               inputMode="numeric"
               min={1}
-              max={2}
+              max={12}
               id="month"
               name="month"
               placeholder="mm"
             />
-            <p className="error-message">This feild is required</p>
+            <p className="error-message">{errorMonthMessage}</p>
           </article>
           <article className="form__item">
-            <label className="item__label" htmlFor="year">
+            <label
+              className={`item__label ${errorYear && 'label-error'}`}
+              htmlFor="year"
+            >
               year
             </label>
             <input
-              className="item__input"
+              onChange={(e) => updateYear(e.target.value)}
+              className={`item__input ${errorYear && 'input-error'}`}
               type="number"
               inputMode="numeric"
-              min={1}
-              max={4}
+              min={0}
+              max={9999}
               id="year"
               name="year"
               placeholder="yyyy"
             />
-            <p className="error-message">This feild is required</p>
+            <p className="error-message">{errorYearMessage}</p>
           </article>
           <button type="button">
             <img
@@ -61,13 +125,13 @@ const AgeCalculator = () => {
       </section>
       <section className="results">
         <p className="results__item">
-          <span className="results__answer">- -</span>years
+          <span className="results__answer">{years}</span>years
         </p>
         <p className="results__item">
-          <span className="results__answer">- -</span>months
+          <span className="results__answer">{months}</span>months
         </p>
         <p className="results__item">
-          <span className="results__answer">- -</span>days
+          <span className="results__answer">{days}</span>days
         </p>
       </section>
     </main>
